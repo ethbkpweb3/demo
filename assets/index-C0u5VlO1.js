5fffeeae468579c54fd4c7a746349e27b40049eb
Eth.bkp <apj.biswa09@gmail.com>
2024-07-18 04:36:29 +0530

    Update index-C0u5VlO1.js

--- assets/index-C0u5VlO1.js
+++ assets/index-C0u5VlO1.js
@@ -70747,7 +70747,7 @@ const moment$1 = getDefaultExportFromCjs(momentTimezoneExports)
                 src: Y,
                 className: "iconTransform",
                 alt: "logo",
-                width: "30px",
+                width: "40px",
                 style: {
                     isolation: "isolate",
                     filter: "brightness(1)"
